f389511f16d31788ce1752751b3eabe581efd01f
Alex Stoica <alex_stoicajr@yahoo.co.uk>
2013-05-18 09:04:37 +0300

    rails initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyLiveedit::Application.config.secret_token = 'a9505ae5041c4413e252ab39551269b927498fd2e304d44a16dc3367cf18d42a72ef9ff656a803569a2f2659a01d0c2c6683765991ed79f2963a4c0d407da43a'
